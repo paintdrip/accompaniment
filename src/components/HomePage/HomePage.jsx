ace9cd5e234fd194ff83bf30c883886e8a6eca05
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-06-13 17:09:55 +0300

    destructuring the first 'homepage' component

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import mainLogo from '../../images/mainLogo.png';
+import play from '../../images/play.png';
+
+const HomePage = () => {
+  return (
+    <section className="welcomePage">
+      {/* Верхний блок */}
+      <div className="welcomePage-header">
+        <img src={mainLogo} alt="логотип сайта" className="welcomePage-header-img" />
+        <h1 className="welcomePage-header-title">АККОМПАНЕМЕНТ</h1>
+        <div className="welcomePage-header-subtitle">
+          <img src={play} alt="иконка play" className="welcomePage-header-subtitle-img" />
+          <p className="welcomePage-header-subtitle-text">ДЛЯ ВСЕХ</p>
+        </div>
+      </div>
+      {/* Нижний блок */}
+      <div className="welcomePage-description">
+        <h4 className="welcomePage-description-text">
+          ЗДЕСЬ ГОТОВЯТСЯ СОЛИСТЫ <br />
+          ДЛЯ БОЛЬШОЙ СЦЕНЫ
+        </h4>
+        <img src={play} alt="иконка play" className="welcomePage-description-img" />
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
