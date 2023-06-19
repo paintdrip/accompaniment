1e51a1795a16abb4fbfe2554938d3645c7334dab
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-06-19 17:56:26 +0300

    some test layouts

--- src/pages/AudioPlayer.jsx
+++ src/pages/AudioPlayer.jsx
@@ -16,45 +16,66 @@ import 'rc-slider/assets/index.css';
 
 export const AudioPlayer = () => {
   return (
-    <section className="audioplayer">
-      <Header />
-      <div className="audioplayer-main">
-        {/* основной логотип */}
-        <img src={logo_accomp_full} alt="логотип сайта" className="audioplayer-main-logo" />
-        {/* карточка для аккомпанемента */}
-        <div className="audioplayer-main-musiccard">
-          <img
-            src={musiccard_logo}
-            alt="карточка аккомпанемента"
-            className="audioplayer-main-musiccard-logo"
-          />
-          <b className="audioplayer-main-musiccard-name">
-            Концерт № 1 для скрипки и струнных (ля минор) BWV 1041
-          </b>
-          <b className="audioplayer-main-musiccard-composer">Иоганн Себастьян Бах</b>
-          {/* слайдер, кнопки и выбор темпа */}
-          <container className="slider">
-            {/* слайдер */}
-            <div className="slider-range">
-              <p>2:47</p>
-              <Slider className="slider-range-strip" />
-              <p>5:28</p>
-            </div>
-            {/* переключатели */}
-            <div className="slider-switches">
-              <img src={left_button} alt="кнопка прокрутки влево" className="slider-switches-btn" />
-              <img src={play_button} alt="кнопка запуска" className="slider-switches-btn-main" />
-              <img src={looping_button} alt="кнопка зацикливания" className="slider-switches-btn" />
-            </div>
-            {/* выбор темпа */}
-            <div className="slider-temps">
-              <img src={temp_100} alt="темп 100" className="slider-temps-icon" />
-              <img src={temp_112} alt="темп 112" className="slider-temps-icon" />
-              <img src={temp_120} alt="темп 120" className="slider-temps-icon" />
-            </div>
-          </container>
+    <>
+      <section className="audioplayer">
+        <Header />
+        <div className="audioplayer-main">
+          {/* основной логотип */}
+          <img src={logo_accomp_full} alt="логотип сайта" className="audioplayer-main-logo" />
+          {/* карточка для аккомпанемента */}
+          <div className="audioplayer-main-musiccard">
+            <img
+              src={musiccard_logo}
+              alt="карточка аккомпанемента"
+              className="audioplayer-main-musiccard-logo"
+            />
+            <b className="audioplayer-main-musiccard-name">
+              Концерт № 1 для скрипки и струнных (ля минор) BWV 1041
+            </b>
+            <b className="audioplayer-main-musiccard-composer">Иоганн Себастьян Бах</b>
+            {/* слайдер, кнопки и выбор темпа */}
+            <container className="slider">
+              {/* слайдер */}
+              <div className="slider-range">
+                <p>2:47</p>
+                <Slider className="slider-range-strip" />
+                <p>5:28</p>
+              </div>
+              {/* переключатели */}
+              <div className="slider-switches">
+                <img
+                  src={left_button}
+                  alt="кнопка прокрутки влево"
+                  className="slider-switches-btn"
+                />
+                <img src={play_button} alt="кнопка запуска" className="slider-switches-btn-main" />
+                <img
+                  src={looping_button}
+                  alt="кнопка зацикливания"
+                  className="slider-switches-btn"
+                />
+              </div>
+              {/* выбор темпа */}
+              <div className="slider-temps">
+                <img src={temp_100} alt="темп 100" className="slider-temps-icon" />
+                <img src={temp_112} alt="темп 112" className="slider-temps-icon" />
+                <img src={temp_120} alt="темп 120" className="slider-temps-icon" />
+              </div>
+            </container>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="description">
+        <button className="description-btn">▶ СКАЧАТЬ НОТЫ</button>
+        {/* иконка */}
+        {/* надпись авторства */}
+        {/* описание */}
+        <div>{/* изображение композитора */}</div>
+        <div>
+          {/* фио композитора */}
+          {/* описание */}
+        </div>
+      </section>
+    </>
   );
 };
