f5225bfb48c8025d54003c3752068f192d332e67
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-06-26 11:43:19 +0300

    create stub for the error page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  return <div>Произошла ошибка</div>;
+  return (
+    <section className="not-found-page">
+      <div className="not-found-page__main">
+        <h1 className="not-found-page__main__text">Произошла ошибка 😔</h1>
+        <Link to="/">
+          <button className="not-found-page__main__btn">Вернуться на главную</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
